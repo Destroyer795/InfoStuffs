e665dc0f4abdfcde54a38ce6b3561864ff773a1b
Pranav Kishan <149817232+Destroyer795@users.noreply.github.com>
2025-08-03 16:24:27 +0530

    Cleaned info.route.js

--- backend/routes/info.route.js
+++ backend/routes/info.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createInfo, deleteInfo, getInfos, updateInfo } from '../controller/info.controller.js';    // Import the controller functions for handling info operations
+import { createInfo, deleteInfo, getInfos, updateInfo } from '../controller/info.controller.js';
 import { requireAuth } from '../middleware/requireAuth.js';
 
 const router = express.Router(); //create a new router instance    
@@ -7,15 +7,11 @@ const router = express.Router(); //create a new router instance
 router.use(requireAuth); //apply authentication middleware to all routes in this router
 //this middleware ensures that the user is authenticated before accessing any of the routes defined below
 //the requireAuth middleware checks if the user is authenticated and can be used to protect routes from unauthorized access
-//if the user is not authenticated, they will be redirected or receive an error response
-//this is useful for routes that require user authentication, such as creating, updating, or deleting
+//if the user is not authenticated, they will be redirected
 
 router.get('/', getInfos); //route to get all infos
 router.post('/', createInfo); //route to create a new info
 router.patch('/:id', updateInfo); //route to update an existing info by ID
 router.delete('/:id', deleteInfo); //route to delete an info by ID
 
-export default router; // Export the router to use in other parts of the application
-//this file defines the routes for handling information-related operations in the application.
-//it uses Express.js to create a router and defines routes for getting, creating, updating, and deleting info documents.
-//the router is then exported to be used in the main application file.
+export default router;
