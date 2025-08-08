5be3961729cb79f1e2e95b8d395f7a7802d868cb
Destroyer795 <ty.pranavkishan7905@gmail.com>
2025-08-08 15:57:27 +0530

    Modified sign up card

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export default function Signup() {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <Stack direction="column" spacing={2} sx={{ textAlign: 'center' }} width={{ sm: '25rem', xs: '90%' }} height={{ sm: '35rem', xs: '90%' }} p={4} borderRadius={2} boxShadow={3} bgcolor="background.paper" ml={2} mr={2}>
-                <Typography variant="h4" sx={{ mb: 2 }}>You are at the right place to store your important data to review back later!</Typography> 
-                <Typography variant="body1" sx={{ mb: 4 }}>You can store images, texts, and even files that can be reviewed whenever needed.</Typography>
+            <Stack direction="column" spacing={2} sx={{ textAlign: 'center' }} width={{ sm: '25rem', xs: '90%' }} height={{ sm: '43.3rem', xs: '90%' }} p={4} borderRadius={2} boxShadow={3} bgcolor="background.paper" ml={2} mr={2}>
+                <Typography variant="h4" sx={{ mb: 2 }}>Your personal space for everything that matters.</Typography> 
+                <Typography variant="body1" sx={{ mb: 4 }}>Easily save memories, notes, and files. Access them anytime, anywhere, safely and securely.</Typography>
             </Stack>
             <SignUp signInUrl="/login" forceRedirectUrl={"/"}/>
         </Box>
